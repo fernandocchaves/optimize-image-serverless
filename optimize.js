544a805b2db294aee5816a3a9e88b21268e129aa
Fernando Cezar chaves <fernandocchaves@gmail.com>
2020-09-24 22:37:05 -0300

    initial commit

---
+++ optimize.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const S3 = new AWS.S3()
+const Bucket = process.env.bucket
+const sharp = require('sharp');
+const { basename, extname } = require('path')
+
+module.exports.handle = async ({ Records: records }, context) => {
+  try {
+    console.log(records)
+
+    await Promise.all(
+      records.map(async record => {
+        const { key } = record.s3.object;
+        const srcKey = decodeURIComponent(key.replace(/\+/g, " "));
+
+        const image = await S3.getObject({
+          Bucket,
+          Key: srcKey
+        }).promise();
+
+        const optimized = await sharp(image.Body)
+          .resize(1280, 720, { fit: 'inside', withoutEnlargement: true })
+          .toFormat('jpeg', { progressive: true, quality: 50 })
+          .toBuffer();
+
+        await S3.putObject({
+          Body: optimized,
+          Bucket,
+          ContentType: 'image/jpeg',
+          Key: `compressed/${basename(key, extname(key))}.jpg`
+        }).promise();
+      })
+    )
+
+    return {
+      statusCode: 301,
+      body: {}
+    }
+  } catch (error) {
+    console.log(error)
+    return error;
+  }
+};
